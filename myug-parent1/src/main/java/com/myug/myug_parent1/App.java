f934f093bd33d85cd810c2620ba304bed444aa2d
Umesh <Umesh@Umesh-PC>
2016-11-23 00:18:13 +0530

    first commit

---
+++ myug-parent1/src/main/java/com/myug/myug_parent1/App.java
@@ -0,0 +1,13 @@
+package com.myug.myug_parent1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
